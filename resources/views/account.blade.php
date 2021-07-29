a4ebcfcdcff8f9fd83a91f0bff18afe89ba1f4f0
Sabrina Sumona <snsbauet04@gmail.com>
2021-07-29 17:24:22 +0600

    Shortcut icon added

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Products | RedStore</title>
+    <link rel="shortcut icon" type="images/png" href="images/logo.png" sizes="16x16">
     <link rel="stylesheet" href="{{ url('/css/style.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
           rel="stylesheet">
